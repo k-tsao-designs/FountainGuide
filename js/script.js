78d64303f00630c376c1916d3cbae8428508c878
k-tsao-designs <tsaok49@gmail.com>
2016-12-03 20:51:19 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-	// $("#landing-page").animate({opacity: '0'}, 4000)
+	$("#landing-page").animate({opacity: '0'}, 4000)
 
- //    function hideText() {
- //        $("#landing-page").css({display: "none"});
- //    }
+    function hideText() {
+        $("#landing-page").css({display: "none"});
+    }
 
- //    setTimeout(hideText,4000);
+    setTimeout(hideText,4000);
 
 
 //Open/Close buttons
@@ -16,9 +16,144 @@ $(document).ready(function() {
 		// $("info").animate({height:"600px"}, 1500);
 	});
 
-	$('#pickle-btn').click(function(){
+	$('#picklesoup-btn').click(function(){
 		$(".info").css({display:"none"})
-		$("#pickle-info").css({display:"block"});
+		$("#picklesoup-info").css({display:"block"});
+	});
+
+	$('#killian-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#killian-info").css({display:"block"});
+	});
+
+	$('#martini-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#martini-info").css({display:"block"});
+	});
+
+	$('#birthday-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#birthday-info").css({display:"block"});
+	});
+
+	$('#mural-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#mural-info").css({display:"block"});
+	});
+
+	$('#fountain-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#fountain-info").css({display:"block"});
+	});
+
+	$('#inclusivity-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#inclusivity-info").css({display:"block"});
+	});
+
+	$('#kitchen-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#kitchen-info").css({display:"block"});
+	});
+
+	$('#soaphospital-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#soaphospital-info").css({display:"block"});
+	});
+
+	$('#goodpeople-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#goodpeople-info").css({display:"block"});
+	});
+
+	$('#oddroom-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#oddroom-info").css({display:"block"});
+	});
+
+	$('#lightbox-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#lightbox-info").css({display:"block"});
+	});
+
+	$('#furniture-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#furniture-info").css({display:"block"});
+	});
+
+	$('#lampshade-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#lampshade-info").css({display:"block"});
+	});
+
+	$('#bathroom-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#bathroom-info").css({display:"block"});
+	});
+
+	$('#marriage-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#marriage-info").css({display:"block"});
+	});
+
+	$('#barbara-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#barbara-info").css({display:"block"});
+	});
+
+	$('#distillery-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#distillery-info").css({display:"block"});
+	});
+
+	$('#maltmistake-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#maltmistake-info").css({display:"block"});
+	});
+
+	$('#drinkid-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#drinkid-info").css({display:"block"});
+	});
+
+	$('#orangepeel-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#orangepeel-info").css({display:"block"});
+	});
+
+	$('#pricing-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#pricing-info").css({display:"block"});
+	});
+
+	$('#tourism-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#tourism-info").css({display:"block"});
+	});
+
+	$('#beefmelt-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#beefmelt-info").css({display:"block"});
+	});
+
+	$('#chili-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#chili-info").css({display:"block"});
+	});
+
+	$('#cedarcrest-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#cedarcrest-info").css({display:"block"});
+	});
+
+	$('#family-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#family-info").css({display:"block"});
+	});
+
+	$('#value-btn').click(function(){
+		$(".info").css({display:"none"})
+		$("#value-info").css({display:"block"});
 	});
 
 	$('.closeinfo').click(function(){
